4972b217a36913d8edd48af89b1e8b0232f0d90a
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-02-04 18:32:17 -0300

    exercício 07 js
    comando iniciais do exercício 07

--- cursoJs/exercício 07/main.js
+++ cursoJs/exercício 07/main.js
@@ -1,9 +1,41 @@
 var num = window.document.getElementById('txtn')
+var sel = window.document.getElementById('seltab')
 
 function adicionar(){
+    
+    Number(num.value)
+    if (num.value.lenght == 0 || num.value <= 0 || num.value >100  ) {
 
+            window.alert('Digite um valor válido!')
 
-    window.alert(Number(num.value))
+    }else {
+
+        
+            let item = document.createElement('option')
+            item.text = `O valor digitado é : ${Number(num.value)}`
+            sel.appendChild(item)
+            var vet = []
+            vet.length = 10
+            
+            
+            /*
+                var c = 0
+                if(c<=vet.length){
+                vet.push = Number(num.value)
+                c++
+            }
+            
+            for(let c = 0; c<vet.length; c++){
+                vet[c] = Number(num.value)
+                window.alert(vet)
+
+            }*/
+
+                        
+
+    }
+    
+        window.alert(vet)
 
 }
 
