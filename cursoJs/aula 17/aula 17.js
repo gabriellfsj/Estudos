ed618c84e44ebb4877084a150b90348ce65dddd5
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-02-04 20:53:12 -0300

    aula 17 parte final
    Finalização do exercício da ultima aula do curso.

---
+++ cursoJs/aula 17/aula 17.js
@@ -0,0 +1,13 @@
+
+let amigo = {
+    nome: 'josé',
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p=0){
+        console.log('engordou!')
+        this.peso += p
+    }
+}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} peso ${amigo.peso}kg`)
--- cursoJs/exercício 07/main.js
+++ cursoJs/exercício 07/main.js
@@ -1,5 +1,8 @@
 var num = window.document.getElementById('txtn')
 var sel = window.document.getElementById('seltab')
+var res = window.document.getElementById('res')
+var vet = []
+
 
 function adicionar(){
     
@@ -10,39 +13,41 @@ function adicionar(){
 
     }else {
 
-        
+            vet.push(Number(num.value))
             let item = document.createElement('option')
-            item.text = `O valor digitado é : ${Number(num.value)}`
+            item.text = `O valor adicionado é : ${Number(num.value)}`
             sel.appendChild(item)
-            var vet = []
-            vet.length = 10
-            
-            
-            /*
-                var c = 0
-                if(c<=vet.length){
-                vet.push = Number(num.value)
-                c++
-            }
-            
-            for(let c = 0; c<vet.length; c++){
-                vet[c] = Number(num.value)
-                window.alert(vet)
-
-            }*/
-
-                        
+               
+                                  
 
     }
     
-        window.alert(vet)
-
+        
 }
 
 function calcular(){
 
+        res.innerHTML=" "
+        vet.push()
+        let ultimo = vet[vet.length - 1]
+        let primeiro = vet[0]
+        
+        res.innerHTML +=` O maior valor digitado foi: ${ultimo} </br>`
+
+        res.innerHTML +=` O menor valor digitado foi: ${primeiro} </br>`
+
+        res.innerHTML += ` A soma dos valores digitados é: ${vet.reduce(somar, 0)}</br>`
+
+        res.innerHTML += ` A média dos valores digitados é: ${vet.reduce(somar, 0)/vet.length}</br>`
+
+        res.innerHTML += ` A quantidades de valores adicionado é: ${vet.length}`
+
 
 
 
 
 }
+
+function somar(total, number) {
+    return total + number
+  }
