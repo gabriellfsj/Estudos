3e0f899e76a84cc5c5081c3a93949a004249a9b5
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-02-21 16:52:18 -0300

    cod3r cap2

---
+++ Curso cod3r/Javascript/Capítulo_02_Variáveis/NumerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+//Cuidados ao trabalhar com números 
+
+console.log(7/0)
+console.log('10' /2)
+console.log("show"*2)
+console.log(0.1 + 0.7)
+// não pode pois tem que respeitar o formato console.log(10.toString())
+console.group((10.345).toFixed(2))
---
+++ Curso cod3r/Javascript/Capítulo_02_Variáveis/math.js
@@ -0,0 +1,8 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+
+console.log(area)
+console.log(typeof Math)
+
+//math é um objeto que possui uma constante pi e que permite fazer cálculos de potencência com o 'pow'
