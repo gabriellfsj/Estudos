80afc5e8cd44344aea9d43344d1173f681696403
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-03-21 20:14:40 -0300

    cod3r class

---
+++ Curso cod3r/Javascript/Função/funcaoConstrutora.js
@@ -0,0 +1,9 @@
+function carro( velocidadeMaxima = 200, delta = 5){
+    // Atributo brivado 
+    let velocidadeAtual = 0
+
+    //metodo publico
+    this.acelerar = ()=> {
+        if(velocidadeMaxima)
+    }
+}
