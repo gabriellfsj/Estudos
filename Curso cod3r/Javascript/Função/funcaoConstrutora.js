066556ee2e95cb60fc0a3be543a2cc9945c44629
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-03-21 21:02:06 -0300

    cod3r class

--- Curso cod3r/Javascript/Função/funcaoConstrutora.js
+++ Curso cod3r/Javascript/Função/funcaoConstrutora.js
@@ -1,9 +1,36 @@
-function carro( velocidadeMaxima = 200, delta = 5){
-    // Atributo brivado 
+function carro(velocidadeMaxima = 200, delta = 5){
+
+    //Atributo privado 
     let velocidadeAtual = 0
 
-    //metodo publico
-    this.acelerar = ()=> {
-        if(velocidadeMaxima)
+    //Método publico
+    this.acelerar = () => {
+        if(velocidadeAtual + delta <= velocidadeMaxima){
+            velocidadeAtual += delta
+        }else{
+            velocidadeAtual = velocidadeMaxima
+        }
     }
+
+    //Método Publico
+    this.getVelocidadeAtual = () => {
+        return velocidadeAtual
+    }
+
 }
+
+const uno = new carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new carro(350,20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof carro)
+console.log(typeof ferrari)
+
+
+
---
+++ Curso cod3r/Javascript/Função/tiposDeclaracao.js
@@ -0,0 +1,18 @@
+// Function declaretion 
+
+function soma(x,y){
+    return x + y
+}
+
+// Function expression
+
+const sub = function(x,y){
+    return x-y
+}
+
+// Named function expression
+
+const mult = function mult(x,y){
+    return x*y
+}
+
