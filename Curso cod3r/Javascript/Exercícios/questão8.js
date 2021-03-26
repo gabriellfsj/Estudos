2116c0fa4f1efda8818286b63361f7e70e80ad65
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-03-26 15:17:22 -0300

    react class

---
+++ Curso cod3r/Javascript/Exercícios/questão8.js
@@ -0,0 +1,12 @@
+function cadastrodePartida (valores){
+    valores = []
+    valores.push(valores)
+    console.log(valores)
+
+}
+
+cadastrodePartida(12)
+cadastrodePartida(10)
+cadastrodePartida(08)
+
+
--- ReactJs_curso/site1/src/index.js
+++ ReactJs_curso/site1/src/index.js
@@ -2,11 +2,19 @@ import './index.css'
 import React from "react"
 import ReactDOM from 'react-dom'
 
+import comParamentro from './components/comParamentro'
 import Primeiro from './components/primeiro'
 
-ReactDOM.render(
 
-    <Primeiro></Primeiro>,
+ReactDOM.render(
+    <div>
+        <comParamentro titulo="Esse é um título" subtitulo="Esse é o subtítulo"/>
+        <Primeiro></Primeiro>
+        
+        
+
+    </div>
+    ,
     
     document.getElementById('root')
 
