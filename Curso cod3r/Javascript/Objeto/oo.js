e0a343bac946a7c321276469e0e439ab65b99a0a
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-03-30 16:47:47 -0300

    cod3r class

---
+++ Curso cod3r/Javascript/Objeto/oo.js
@@ -0,0 +1,26 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()// Foco passou a ser o objeto
+
+/*
+Principios importantes:
+    1. Abstração
+    2. Encapisulamento
+    3. Herança(propotype)
+    4. Polimorfismo
+    
+*/
