0089adabce2324b5aef6b2acccb118d27f824a69
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-05-23 20:29:56 -0300

    cod3r class

--- Curso_cod3r/Javascript/Node/projeto/src/servidor.js
+++ Curso_cod3r/Javascript/Node/projeto/src/servidor.js
@@ -1,14 +1,31 @@
 const porta = 3003
 
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('./body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res,next)=>{
-    res.send({nome: 'Notebook', preco: 123.45}) // converter para JSON
+    res.send(bancoDeDados.getProdutos()) // converter para JSON
+})
+
+app.get('/produtos/:id', (req, res,next)=>{
+    res.send(bancoDeDados.getProduto(req.params.id)) // converter para JSON
+})
+
+app.post('/produtos', (req, res, next)=>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)//JSON
 })
 
 app.listen(porta,()=>{
-    console.log(`servidor está executando na porta ${porta}`)
+    console.log(`servidor está e
+    xecutando na porta ${porta}`)
 })
 
