8d6848cbd06a1a079cf6537af2111e8894732ebc
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-06-04 19:13:51 -0300

    cod3r class

---
+++ Curso_cod3r/Javascript/esnext/promisesAnatomia.js
@@ -0,0 +1,22 @@
+// Criando uma promise
+
+const p = new Promise((resolve, reject) =>{
+    try{
+        resolver (funcaoX())
+    }catch(e){
+        reject(e)
+    }
+})
+
+
+// Executando a Promise
+
+p
+.then((parametros) => /* sucesso */)
+.catch((erro) => /* erro */)
+
+
+// Tratando erros e sucessos com then
+
+.then(resposta => { /* tratar resposta */ }, erro => { /* tratar erro */ })
+
