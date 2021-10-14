7559f425a0778ce93cea75c43c62dea6f8ebfdda
Gabriellfsj <gabriellfsj@gmail.com>
2021-10-14 12:22:12 -0300

    JUnit
    Aula de DIO de JUnit

---
+++ JUnit projeto/src/test/java/com/teste/AssertTest.java
@@ -0,0 +1,59 @@
+package com.teste;
+
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+
+
+
+public class AssertTest {
+
+    @Test
+    public void testAssertArrayEquals(){
+        byte[] esperado = "teste".getBytes();
+        byte[] atual = "teste".getBytes();
+        assertArrayEquals(esperado, atual);
+
+    }
+
+    @Test
+    public void testAssertEquals(){
+        assertEquals("text", "text");
+    }
+
+    @Test
+    public void testAssertFalse(){
+        assertFalse(false);
+    }
+
+    @Test
+    public void testAssertNotNull(){
+        assertNotNull(new Object());
+    }
+
+    @Test
+    public void testAssertNotSame(){
+        assertNotSame(new Object(), new Object());
+    }
+
+    @Test
+    public void testAssertNull(){
+        assertNull(null);
+    }
+
+    @Test
+    public void testAssertSame(){
+        Integer aNumber = Integer.valueOf(765);
+        assertSame(aNumber, aNumber);
+    }
+
+
+}
---
+++ JUnit projeto/src/test/java/com/teste/ExceptionTest.java
@@ -0,0 +1,30 @@
+package com.teste;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExceptionTest {
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void empty(){
+        new ArrayList<Object>().get(0);
+    }
+
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shouldTestExceptionMessage() throws IndexOutOfBoundsException{
+        List<Object> list = new ArrayList<Object>();
+
+        thrown.expect(IndexOutOfBoundsException.class);
+        thrown.expectMessage("Index: 0, Size: 0");
+        list.get(0);
+    }
+}
